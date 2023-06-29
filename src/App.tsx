6259186c689923b512942330ec106f198554e224
ITPLUS Academy <thanghoang064@gmail.com>
2023-06-29 18:11:14 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Counter></Counter>
+          {/* <Counter></Counter> */}
+          <Login></Login>
       </header>
     </div>
   );
---
+++ src/components/Login.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const Login = () => {
+    const [email,setEmail] = useState<string>('');
+    // khai báo 1 state errors dảng mảng 
+    const [errors,setErrors] = useState<string[]>([]);
+    const handleSubmit = () => {
+       // tạo ra 1 mảng lỗi mới 
+       console.log(123);
+       let newErrors :string[] = [];
+       if(!email || email.length === 0) {
+            setErrors([]);
+            newErrors.push("Email không được để trống");
+       } else if (email.length < 6){ // chiều dài email phải lớn hơn 6 ký tự
+            setErrors([]);
+            newErrors.push("Chiều dài email phải lớn hơn 6 ký tự ");
+       }
+       // set đống newErrors này vào trong state errors
+
+       setErrors((prevErrors)=> {
+            const uniqueErrors = new Set([...prevErrors,...newErrors]);
+            return Array.from(uniqueErrors);
+       }) // lọc xong những phần tử trùng và sẽ 1 mảng lỗi mới vào trong 
+       //state errors
+
+    }
+    return(
+        <>
+        <h1>Đăng nhập</h1>
+        <form>
+            <label>Email
+                <input type="text" onChange={(event)=> setEmail(event.target.value)}/>
+            </label>
+            <button type="button" onClick={handleSubmit}>Gửi</button>
+            {
+                errors.length > 0 && (
+                    <ul>
+                        {
+                           errors.map((error,index)=>(
+                            <li key={index}>{error}</li>
+                           )) 
+                        }
+                    </ul>    
+                )
+            }
+        </form>
+        </>
+    );
+}
+export default Login;
